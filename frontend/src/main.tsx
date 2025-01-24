b8255d2655b3899ab1bbc8817ffe033fa4c3a45b
Sanzhar Beishekeev <franki-55@mail.ru>
2025-01-25 00:35:09 +0600

    [Exam-11] Сделал init для фронта и бека, а также сделал роуты и модели для user как на беке так и на фронте, на фронте добавил форму авторизации и навбар

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+
+createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
+    </Provider>,
+);
